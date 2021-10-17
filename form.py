75541b6d48bc6cc6176355c59cdf997465c5527e
Breketov <57558717+Breketov@users.noreply.github.com>
2021-10-17 15:11:38 +0300

    Test + Osnovnaya1

--- form.py
+++ form.py
@@ -1,1 +1,22 @@
-import main
+""" from math import comb
+from tkinter import *
+from tkinter.ttk import Combobox  
+
+window = Tk()
+window.title("Лабораторная №1 по ЧМ")
+window.geometry('1024x720')
+
+
+zadachi = Combobox(window, state='readonly')
+zadachi['values'] = ['Тестовая', 'Основная №1', 'Основная №2']
+zadachi.current(0)
+zadachi.grid(column = 0, row = 0)
+
+
+
+window.mainloop()
+
+
+btn = Button(window, )
+
+ """
--- main.py
+++ main.py
@@ -1,20 +1,56 @@
+from typing import ValuesView
 import numpy as np
 from numpy.core.function_base import linspace
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
-import cmath
+import math
 
-u0 = float(input("Введите начальное условие u при х = 0: "))
 
-def test(u, x):
-   dudx = -3/2 * u
-   return dudx
+n = 5
+step = 0.01
+x = 0
+v = 2
 
-x = np.linspace(0, 5, 100)
-u = odeint(test, u0, x)
+class zadachi():
+    def __init__(self, x, u) -> None:
+        self.x = x
+        self.u = u
+        pass
+    def test(x, u):
+        dudx = -1* 3/2 * u
+        return dudx
+    
+    def osnovnay1(x, u):
+        dudx = 1/((1+(x**2))**(1/3))*(u**2) + u - (u**3)*math.sin(10*x)
+        return dudx
 
-plt.plot(x, u, 'o-', linewidth = 2.0)
-plt.xlabel('x')
-plt.ylabel('u(x)')
-plt.grid()
-plt.show()
+    """ def osnovnaya2(x, u, u0, u_0): """
+
+
+
+
+
+for i in range(1, n+1):
+    k1 = zadachi.test(x, v)
+    k2 = zadachi.test(x + step / 2.0, v + 0.5 * step * k1)
+    k3 = zadachi.test(x + step / 2.0, v + 0.5 * step * k2)
+    k4 = zadachi.test(x + step, v + step * k3)
+    x = x + step
+    v = v + step/6 * (k1 + 2*k2 + 2*k3 + k4)
+    print(x, '   ', v)
+
+print('___________________________________________')
+
+n = 5
+step = 0.01
+x = 0
+v = 2
+
+for i in range(1, n+1):
+    k1 = zadachi.osnovnay1(x, v)
+    k2 = zadachi.osnovnay1(x + step / 2.0, v + 0.5 * step * k1)
+    k3 = zadachi.osnovnay1(x + step / 2.0, v + 0.5 * step * k2)
+    k4 = zadachi.osnovnay1(x + step, v + step * k3)
+    x = x + step
+    v = v + step/6 * (k1 + 2*k2 + 2*k3 + k4)
+    print(x, '   ', v)

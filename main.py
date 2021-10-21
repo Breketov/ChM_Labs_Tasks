7977b39ccaf6e6c22b2abf7365ced373e0c86b3f
Breketov <57558717+Breketov@users.noreply.github.com>
2021-10-22 00:49:48 +0300

    Теперь рисуются графики

--- main.py
+++ main.py
@@ -19,13 +19,15 @@ class zadachi():
     def osnov2(x, u):
         du_ = u[1]
         dx_= -a* (u[1]**2) - b * u[0]
-        return [du_, dx_] 
+        return du_, dx_ 
 
 class RK4(zadachi):
     def __init__(self) -> None:
         pass
 
     def RK4_test(x, v, step, n):
+        x_ = []
+        v_ = []
         print(x, '   ', v)
         for i in range(1, n+1):
             k1 = zadachi.test(x, v)
@@ -35,10 +37,15 @@ class RK4(zadachi):
 
             x = x + step
             v = v + step/6 * (k1 + 2*k2 + 2*k3 + k4)
+
+            x_.append(x)
+            v_.append(v)
             print(x, '   ', v)
-        return [x, v] 
+        return x_, v_
 
     def RK4_osnov1(x, v, step, n):
+        x_ = []
+        v_ = []
         print(x, '   ', v)
         for i in range(1, n+1):
             k1 = zadachi.osnov1(x, v)
@@ -48,9 +55,16 @@ class RK4(zadachi):
 
             x = x + step
             v = v + step/6 * (k1 + 2*k2 + 2*k3 + k4)
+
+            x_.append(x)
+            v_.append(v)
             print(x, '   ', v)
+        return x_, v_
 
     def RK4_osnov2(x, v, step, n):
+        x_ = []
+        v1_ = []
+        v2_ = []
         print(x, '   ', v)
         for i in range(1, n+1):
             v_ = np.array(v)
@@ -61,45 +75,93 @@ class RK4(zadachi):
             k3 = zadachi.osnov2(x + step / 2, v_ + 0.5 * step * k2_)
             k3_ = np.array(k3)
             k4 = zadachi.osnov2(x + step, v_ + step * k3_)
-            k4_ = np.array(k3)
+            k4_ = np.array(k4)
 
             x = x + step
             v = v_ + step/6 * (k1_ + 2*k2_ + 2*k3_ + k4_)
+
+            x_.append(x)
+            v1_.append(v[0])
+            v2_.append(v[1])
             print(x, '   ', v[0], '   ', v[1])
+        return x_, v1_, v2_
 
 
 a = 3
 b = 2
 
 
+""" 
+v0 = []
+x0 = float(input())
+v0_0 = float(input())
+v0.append(v0_0)
+v0_1 = float(input())
+v0.append(v0_1)
+step = float(input())
+n = int(input())
+
+c = RK4.RK4_osnov2(x0, v0, step, n)
+ """
+
+
+""" d = RK4.RK4_osnov1(0, 2, 0.01, 10)
+print(d)
+ """
+
+
+""" b = RK4.RK4_test(0, 2, 0.01, 5)
+print(b)
+ """
+
+
+#! Графики истинного решения тестовой задачи рисуется и поточечная траектория численного решения тоже существует и рисуется, но их разница незаметна
+#! Текущие точки численной траектории возвращаются
+
+
+
 
 """ 
-c = RK4.RK4_osnov2(x0, v0, step, n)
-print(c)
+u0 = 2
+x = np.linspace(0, 0.05, 6)
+i = 0
+u_test = []
+while i < len(x):
+   u_ = u0 * math.exp(-3/2 * x[i])
+   u_test.append(u_)
+   i = i + 1
 
-d = RK4.RK4_osnov1(0, 2, 0.01, 5)
-print(d)
+
+e = RK4.RK4_test(0, 2, 0.01, 6)
+e_x = e[0]
+e_v = e[1]
+
+plt.plot(e_x, e_v, 'r--', linewidth = 0.5)
+plt.plot(x, u_test, 'b--', linewidth = 0.5)
+plt.axis([0, 0.05, 0, 2])
+plt.show() 
 """
 
-b = RK4.RK4_test(0, 2, 0.1, 5)
 
 
 
+""" 
+g = RK4.RK4_osnov1(0, 2, 0.01, 6)
+g_x = g[0]
+g_v = g[1]
 
-
-#! Все что находится ниже это полная хуета и работает неккоректно, график кривой не похож на то что я вычислил
-#! Сука
-
-def test_(x_, u_):
-    dudx_ = -1* 3/2 * u_
-    return dudx_
-
-u0 = 2
-x = np.linspace(0, 5, 6)
-u_ = odeint(test_, u0, x)
-u_ = np.array(u_).flatten()
-plt.plot(x, u_, 'o-', linewidth = 2.0)
-""" plt.plot(b[:,0], b[:, 1]) """
+plt.plot(g_x, g_v, 'r--', linewidth = 0.5)
 plt.show()
+ """
 
 
+
+""" 
+l = RK4.RK4_osnov2(0, [3, 2], 0.01, 6)
+l_x = l[0]
+l_v1 = l[1]
+l_v2 = l[2]
+
+plt.plot(l_v1, l_v2, 'r--', linewidth = 0.5)
+plt.show() 
+"""

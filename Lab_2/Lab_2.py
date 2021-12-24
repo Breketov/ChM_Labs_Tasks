0f8df74bed8e583b9b633c713d1f4d80f75d7338
Breketov <evgenii.shmel@mail.ru>
2021-12-24 22:53:18 +0300

    Добавил тестовую задачу | Работает, но есть проблемы с точностью

--- Lab_2/Lab_2.py
+++ Lab_2/Lab_2.py
@@ -1,5 +1,5 @@
 #                       Лабораторная работа №2
-#                       Версия 0.34
+#                       Версия 0.45
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -199,156 +199,6 @@ print(x1)
 
 #_________________________________________________________________________________________
 
-
-
-
-
-n = 10
-
-
-start = 0
-end = 1
-
-h = 1/n
-node = n + 1
-
-my = [1, 0]
-kappa = [0, 0]
-
-ksi = 0.3
-
-
-
-
-
-#! Точное решение задачи
-def true_solution(x):
-   C1 = -2.441089498977808
-   C2 = 0.1077561656444751
-   C3 = 0.8031153039014358
-   C4 = -0.1086898371251591
-   def u1(x):
-      return C1*np.exp(-np.sqrt(ksi/(ksi**2 + 2))*x) + C2*np.exp(np.sqrt(ksi/(ksi**2 + 2))*x) + 1/ksi
-   def u2(x):
-      return C3*np.exp(-x) + C4*np.exp(x) + np.sin(np.pi*x)/((ksi**2)*(1 + np.pi**2)) 
-   x = np.linspace(0, 0.3, 11) 
-   plt.plot(x, u1,'o-', linewidth = 2.0, label='траектория до точки разрыва')
-   x = np.linspace(0.3, 1, 11) 
-   plt.plot(x, u2,'o-', linewidth = 2.0, label='траетория после точки разрыва')
-   plt.legend()
-   plt.xlabel('x')
-   plt.ylabel('u(x)')
-   plt.grid()
-   plt.savefig('График истинной траектории.png')
-   plt.show()
-
-a = np.empty(n)
-d = np.empty(n-1)
-fi = np.empty(n-1)
-
-def node(i, n):
-   return i/(n + 1)
-
-def test_coef(a, d, fi):
-   for i in range(n):
-      if ((node(i, n) < 0.3) and (node(i + 1, n) < 0.3)):
-         a[i] = 2.09
-      elif ((node(i, n) > 0.3) and (node(i + 1, n) > 0.3)):
-         a[i] = 0.09
-      else:
-         a[i] = 1/(n*(0.3 - node(i - 1, n))/2.09 + n*(node(i, n) - 0.3)/0.09)
-
-   for i in range(n - 1):
-      if ((node(i + 0.5, n) < 0.3) and (node(i - 0.5, n) < 0.3)):
-         d[i - 1] = 0.3
-      elif ((node(i + 0.5, n) > 0.3) and (node(i - 0.5, n) > 0.3)):
-         d[i - 1] = 0.09
-      else:
-         d[i - 1] = n*0.3*(0.3 - node(i - 0.5, n)) + n*0.09*(node(i + 0.5, n) - 0.3)
-   
-   for i in range(n - 1):
-      if ((node(i + 0.5, n) < 0.3) and (node(i - 0.5, n) < 0.3)):
-         fi[i - 1] = 1
-      elif ((node(i + 0.5, n) > 0.3) and (node(i - 0.5, n) > 0.3)):
-         fi[i - 1] = np.sin(np.pi*0.3)
-      else:
-         fi[i - 1] = n*(0.3 - node(i - 0.5, n)) + n*np.sin(np.pi*0.3)*(node(i + 0.5, n) - 0.3)
-   return a, d, fi
-
-def test_matrix():
-   a = np.empty(n)
-   d = np.empty(n - 1)
-   fi = np.empty(n - 1)
-   a, d, fi = test_coef(a, d, fi)
-
-   A = np.empty(n - 1)
-   B = np.empty(n - 1)
-   C = np.empty(n - 1)
-   for i in range(n - 1):
-      A[i] = a[i]*((n + 1)**2)
-   for i in range(n - 1):
-      B[i] = a[i + 1]*((n + 1)**2)
-      C[i] = (a[i] + a[i + 1])*((n + 1)**2) + d[i]
-
-def test_progon(A, B, C):
-   #! Прямая прогонка
-   alfa = np.empty(n)
-   beta = np.empty(n)
-   alfa[0] = kappa[0]
-   beta[0] = my[0]
-   for i in range(n - 1):
-      alfa[i + 1] = B[i]/(C[i] - A[i]*alfa[i])
-      beta[i + 1] = (fi[i] + A[i]*B[i])/(C[i] - A[i]*alfa[i])
-   #! Обратная прогонка
-   y = np.empty(n)
-   
-
-
-
-
-
-
-
-
-
-
-
-
-a, d, fi = test_coef(a, d, fi)
-
-print(a)
-print('_______________')
-print(d)
-print('_______________')
-print(fi)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #! Функции
 def k1(x):
    return x**2 + 2
@@ -364,3 +214,127 @@ def f1(x):
    return 1
 def f2(x):
    return np.sin(np.pi*x)
+
+
+#! Точное решение задачи
+def true_solution(x_main):
+   C1 = -2.441089498977808
+   C2 = 0.1077561656444751
+   C3 = 0.8031153039014358
+   C4 = -0.1086898371251591
+   u1, u2 = [], []
+   x1, x2 = [], []
+   for x in x_main:
+      if x <= ksi:
+         u1.append(C1*np.exp(-np.sqrt(ksi/(ksi**2 + 2))*x) + C2*np.exp(np.sqrt(ksi/(ksi**2 + 2))*x) + 1/ksi)
+         x1.append(x)
+      elif x >= ksi:
+         u2.append(C3*np.exp(-x) + C4*np.exp(x) + np.sin(np.pi*x)/((ksi**2)*(1 + np.pi**2)))
+         x2.append(x)
+   plt.plot(x1, u1,'o-', linewidth = 2.0, label='траектория до точки разрыва')
+   plt.plot(x2, u2,'o-', linewidth = 2.0, label='траетория после точки разрыва')
+   plt.legend()
+   plt.xlabel('x')
+   plt.ylabel('u(x)')
+   plt.grid()
+   plt.savefig('График истинной траектории.png')
+   plt.show()
+   return u1 + u2
+
+def node(i, n):
+   return i/n
+
+def coef_matrix(a, d, fi):
+   A = np.empty(n)
+   B = np.empty(n - 1)
+   C = np.empty(n - 1)
+   for i in range(len(a)):
+      A[i] = a[i]*(n**2)
+   for i in range(len(d)):
+      B[i] = a[i + 1]*(n**2)
+      C[i] = (a[i] + a[i + 1])*(n**2) + d[i]
+   return A, B, C
+
+def test_coef(a, d, fi):
+   for i in range(n):
+      if ((node(i, n) <= 0.3) and (node(i + 1, n) <= 0.3)):
+         a[i] = 2.09
+      elif ((node(i, n) >= 0.3) and (node(i + 1, n) >= 0.3)):
+         a[i] = 0.09
+      else:
+         a[i] = 1/(n*(0.3 - node(i - 1, n))/2.09 + n*(node(i, n) - 0.3)/0.09)
+
+   for i in range(n - 1):
+      if ((node(i + 0.5, n) <= 0.3) and (node(i - 0.5, n) <= 0.3)):
+         d[i] = 0.3
+      elif ((node(i + 0.5, n) >= 0.3) and (node(i - 0.5, n) >= 0.3)):
+         d[i] = 0.09
+      else:
+         d[i] = n*0.3*(0.3 - node(i - 0.5, n)) + n*0.09*(node(i + 0.5, n) - 0.3)
+   
+   for i in range(n - 1):
+      if ((node(i + 0.5, n) <= 0.3) and (node(i - 0.5, n) <= 0.3)):
+         fi[i] = 1
+      elif ((node(i + 0.5, n) >= 0.3) and (node(i - 0.5, n) >= 0.3)):
+         fi[i] = np.sin(np.pi*0.3)
+      else:
+         fi[i] = n*(0.3 - node(i - 0.5, n)) + n*np.sin(np.pi*0.3)*(node(i + 0.5, n) - 0.3)
+   return a, d, fi
+
+def test_progonka(A, B, C, a, d ,fi):
+   #! Прямая прогонка
+   alfa = np.empty(n - 1)
+   beta = np.empty(n - 1)
+   alfa[0] = kappa[0]
+   beta[0] = my[0]
+   for i in range(n - 2):
+      alfa[i + 1] = B[i]/(C[i] - A[i]*alfa[i])
+      beta[i + 1] = (fi[i] + A[i]*beta[i])/(C[i] - A[i]*alfa[i])
+   
+   #! Обратная прогонка
+   y = np.empty(n)
+   y[-1] = my[1]
+   for i in range(n - 2, -1, -1):
+      y[i] = alfa[i]*y[i + 1] + beta[i]
+   return y 
+
+def test_task():
+   a = np.empty(n)
+   d = np.empty(n - 1)
+   fi = np.empty(n - 1)
+   a, d ,fi = test_coef(a, d, fi)
+   A, B, C = coef_matrix(a, d, fi)
+   return test_progonka(A, B, C, a, d ,fi)
+
+def solution():
+   h = 1/n
+   x_main = [x_start + i*h for i in range(n + 1)]
+   x_aux = [x_start + (i + 0.5)*h for i in range(n - 1)]
+   v = test_task()
+   v[0] = my[0]
+   u = true_solution(x_main)
+   E = np.abs(u - v)
+
+   Node = [i for i in range(len(x_main))]
+   data_test = {'№ Узла': Node, 'x': x_main, 'v': v, 'u': u, '|u - v|': E}
+   data = pd.DataFrame(data = data_test)
+   data.to_csv("data_test.csv", index=False)
+   
+
+
+#* Терминал
+print('_____________________________________________________________________________________________________________________')
+print('Команда Эльвина | Лабораторная работа №2 | Решение краевых задач для ОДУ')
+print('_____________________________________________________________________________________________________________________')
+print('В рамках данной лабораторной работы решается тествая и основная задачи с определенной сеткой.')
+print('Задайте размерность сетки')
+n = int(input('n = '))
+
+#* Результат работы
+x_start = 0
+x_end = 1
+kappa = [0, 0]
+my = [1, 0]
+ksi = 0.3
+
+solution()

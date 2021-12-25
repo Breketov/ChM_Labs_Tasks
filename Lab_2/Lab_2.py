951bc79e332ee1116d6a6be661f8505f51a32956
Breketov <evgenii.shmel@mail.ru>
2021-12-25 22:31:48 +0300

    Закончил

--- Lab_2/Lab_2.py
+++ Lab_2/Lab_2.py
@@ -1,11 +1,220 @@
-#                       Лабораторная работа №2
-#                       Версия 0.59
-
+#                                Лабораторная работа №2
+#                                Версия 0.5.10
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#! Функции
+#TODO Основные параметры
+x_start = 0
+x_end = 1
+kappa = [0, 0]
+my = [1, 0]
+ksi = 0.3
+
+#!-------------------------------ТЕСТОВАЯ ЧАСТЬ-------------------------------#
+#* Нахождение всех коэффициентов и параметров тестовой задачи
+#* Построение решения
+def test_task(n):
+   h = 1/n
+   x_main = [x_start + i*h for i in range(n + 1)]
+   x_aux = [x_start + (i + 0.5)*h for i in range(n)]
+   a, d ,fi = test_coef(x_main, x_aux, n)
+   A, B, C = coef_matrix(n, a, d, fi)
+         #* Запуск прогонки
+   v = progonka(n, A, B, C, a, d ,fi)
+         #* Истинная траектория
+   u = true_solution(x_main)
+
+      #* Погрешность и запись в дата 
+   E = list(np.abs(np.array(u) - np.array(v)))
+   for i in range(len(x_main)):
+      x_main[i] = round(x_main[i], 13)
+   Node = [i for i in range(len(x_main))]
+
+      #* Данные для справки
+   max_error_E = 0
+   for i in range(len(E)):   
+      if E[i] > max_error_E:
+         max_error_E = E[i]
+   max_E = E.index(max_error_E)
+   x_max = x_main[max_E]
+
+   data_test = {'№ Узла': Node, 'x': x_main, 'v': v, 'u': u, '|u - v|': E}
+   data = pd.DataFrame(data = data_test)
+   data.to_csv("data_test.csv", index=False)
+
+   plot_test(x_main, u, v, E)
+   print('Максимальная погрешность основной задачи: ', max_error_E, '  ', 'при x = ', x_max)
+
+#* Коэффициенты тестовой задачи
+def test_coef(x_main, x_aux, n):
+   a = np.empty(n)
+   for i in range(n):
+      if (x_main[i + 1] <= 0.3):
+         a[i] = 2.09
+      elif (x_main[i] >= 0.3):
+         a[i] = 0.09
+      elif x_main[i] < 0.3 < x_main[i + 1]:
+         a[i] = 1/(1/(2*k1(0.3)) + 1/(2*k2(0.3)))
+
+   d = np.empty(n - 1)
+   for i in range(n - 1):
+      if (x_aux[i + 1] <= 0.3):
+         d[i] = 0.3
+      elif (x_aux[i] >= 0.3):
+         d[i] = 0.09
+      elif x_aux[i] < 0.3 < x_aux[i + 1]:
+         d[i] = (q1(0.3) + q2(0.3))/2
+
+   fi = np.empty(n - 1)
+   for i in range(n - 1):
+      if (x_aux[i + 1] <= 0.3):
+         fi[i] = 1
+      elif (x_aux[i] >= 0.3):
+         fi[i] = np.sin(0.3*np.pi)
+      elif x_aux[i] < 0.3 < x_aux[i + 1]:
+         fi[i] = (f1(0.3) + f2(0.3))/2
+   return a, d, fi
+
+#* Точное решение задачи
+def true_solution(x_list):
+   C1 = -0.9603081818828
+   C2 = -1.3730251514504
+   C3 = -2.4598464169268
+   C4 = -6.2589036085123
+   u1, u2, x1, x2 = [], [], [], []
+   for x in x_list:
+      if x <= ksi:
+         u1.append(C1*np.exp(np.sqrt(ksi/(ksi**2 + 2))*x) + C2*np.exp(-np.sqrt(ksi/(ksi**2 + 2))*x) + 1/ksi)
+         x1.append(x)
+      elif x >= ksi:
+         u2.append(C3*np.exp(x) + C4*np.exp(-x) + np.sin(np.pi*ksi)/0.09)
+         x2.append(x)
+   u = u1 + u2
+   u[0] = my[0]
+   u[-1] = my[1]
+   return u
+
+
+#!-------------------------------ОСНОВНАЯ ЧАСТЬ-------------------------------#
+#* Нахождение всех коэффициентов и параметров основной задачи
+#* Построение решения
+def main_task(n):
+         #* Обычная траектория
+   h = 1/n
+   x_main = [x_start + i*h for i in range(n + 1)]
+   x_aux = [x_start + (i + 0.5)*h for i in range(n)]
+   a, d ,fi = main_coef(x_main, x_aux, n)
+   A, B, C = coef_matrix(n, a, d, fi)
+         #* Запуск прогонки
+   v1 = progonka(n, A, B, C, a, d ,fi)
+         #* Траектория половинного шага
+   h = h/2
+   n = int(2*n)
+   x_main = [x_start + i*h for i in range(n + 1)]
+   x_aux = [x_start + (i + 0.5)*h for i in range(n)]
+   a, d ,fi = main_coef(x_main, x_aux, n)
+   A, B, C = coef_matrix(n, a, d, fi)
+            #* Запуск прогонки
+   v2 = progonka(n, A, B, C, a, d ,fi)[::2]
+   
+         #* Погрешность и запись в дата 
+   e = list(np.abs(np.array(v1) - np.array(v2)))
+   x_main = x_main[::2]
+   for i in range(len(x_main)):
+      x_main[i] = round(x_main[i], 13)
+   Node = [i for i in range(len(x_main))]
+
+         #* Данные для справки
+   max_error_e = 0
+   for i in range(len(e)):
+      if e[i] > max_error_e:
+         max_error_e = e[i] 
+   max_e = e.index(max_error_e)
+   x_max = x_main[max_e]
+
+   data_main = {'№ Узла': Node, 'x': x_main, 'v1': v1, 'v2': v2, '|v2 - v1|': e}
+   data = pd.DataFrame(data = data_main)
+   data.to_csv("data_main.csv", index=False)
+
+   plot_main(x_main, v1, v2, e)
+   print('Максимальная погрешность основной задачи: ', max_error_e, '  ', 'при x = ', x_max)
+
+#* Коэффициенты основной задачи
+def main_coef(x_main, x_aux, n):
+   a = np.empty(n)
+   for i in range(n):
+      if (x_main[i + 1] <= 0.3):
+         a[i] = k1(x_aux[i])
+      elif (x_main[i] >= 0.3):
+         a[i] = k2(x_aux[i])
+      elif x_main[i] < 0.3 < x_main[i + 1]:
+         a[i] = 1/(n*(((0.3 - x_main[i + 1])/(k1((x_main[i] + 0.3)/2))) + ((x_main[i + 1] - 0.3)/(k2((0.3 + x_main[i + 1])/2)))))
+
+   d = np.empty(n - 1)
+   for i in range(n - 1):
+      if (x_aux[i + 1] <= 0.3):
+         d[i] = q1(x_main[i + 1])
+      elif (x_aux[i] >= 0.3):
+         d[i] = q2(x_main[i + 1])
+      elif x_aux[i] < 0.3 < x_aux[i + 1]:
+         d[i] = n*(q1((x_aux[i] + 0.3)/2)*(0.3 - x_aux[i]) + q2((0.3 + x_aux[i + 1])/2)*(x_aux[i + 1] - 0.3))
+   
+   fi = np.empty(n - 1)
+   for i in range(n - 1):
+      if (x_aux[i + 1] <= 0.3):
+         fi[i] = f1(x_main[i + 1])
+      elif (x_aux[i] >= 0.3):
+         fi[i] = f2(x_main[i + 1])
+      elif x_aux[i] < 0.3 < x_aux[i + 1]:
+         fi[i] = n*(f1((x_aux[i] + 0.3)/2)*(0.3 - x_aux[i]) + f2((0.3 + x_aux[i + 1])/2)*(x_aux[i + 1] - 0.3))
+   return a, d, fi
+
+
+#!-------------------------------Функциональая ЧАСТЬ-------------------------------#
+#* Графики Тестовой задачи
+def plot_test(x, u, v, E):
+   #* Численная и истинная траектории
+   plt.plot(x, u, linewidth = 4.0, label='Истинная траектория')
+   plt.plot(x, v, 'x-', linewidth = 1.0, label='Численная траектория')
+   plt.legend()
+   plt.xlabel('x')
+   plt.ylabel('u(x)   v(x)')
+   plt.grid()
+   plt.savefig('График численной и истинной траекторий.png')
+   plt.show()
+
+   #* График разности численнного и аналитического решения
+   plt.plot(x, E, label='Разность численного и аналитичесского решений')
+   plt.legend()
+   plt.xlabel('x')
+   plt.ylabel('|u(x) - v(x)|')
+   plt.grid()
+   plt.savefig('Разность численнного и аналитического решения.png')
+   plt.show()
+
+#* Графики Основной задачи
+def plot_main(x, v1, v2, e):
+   #* Численная и афрро-численная траектории
+   plt.plot(x, v1, linewidth = 4.0, label='Траектория с обычным шагом')
+   plt.plot(x, v2, 'x-', linewidth = 2.0, label='Траектория с половинным шагом')
+   plt.legend()
+   plt.xlabel('x')
+   plt.ylabel('v1(x)   v2(x)')
+   plt.grid()
+   plt.savefig('График численных траекторий.png')
+   plt.show()
+
+   #* График разности численнного и афро-численного решения
+   plt.plot(x, e, label='Разность численных решений')
+   plt.legend()
+   plt.xlabel('x')
+   plt.ylabel('|v1(x) - v2(x)|')
+   plt.grid()
+   plt.savefig('Разность численнных решений решения.png')
+   plt.show()
+
+#* Функции
 k1 = lambda x: x*x + 2
 k2 = lambda x: x*x
 q1 = lambda x: x
@@ -13,14 +222,14 @@ q2 = lambda x: x*x
 f1 = lambda x: 1
 f2 = lambda x: np.sin(np.pi*x)
 
-#! Матричные коэффициенты
+#* Матричные коэффициенты
 def coef_matrix(n, a, d, fi):
    A = np.array([a[i]/((1/n)**2) for i in range(n - 1)])
    B = np.array([a[i]/((1/n)**2) for i in range(1, n)])
    C = np.array([a[i]/((1/n)**2) + a[i + 1]/((1/n)**2) + d[i] for i in range(n - 1)])
    return A, B, C
 
-#! Прогонка
+#* Прогонка
 def progonka(n, A, B, C, a, d ,fi):
    #* Прямая прогонка
    alfa = np.empty(n)
@@ -36,213 +245,20 @@ def progonka(n, A, B, C, a, d ,fi):
    y[-1] = (-kappa[1]*beta[-1] - my[1])/(kappa[1]*alfa[-1] - 1)
    for i in range(n - 2, -1, -1):
       y[i] = alfa[i + 1]*y[i + 1] + beta[i + 1]
+   y = list(y)
+   y.insert(0, 1)
    return y 
-
-#TODO------------------------------------ТЕСТОВАЯ ЗАДАЧА------------------------------------
-#! Точное решение задачи
-def true_plot(x_main):
-   C1 = -0.9603081818828
-   C2 = -1.3730251514504
-   C3 = -2.4598464169268
-   C4 = -6.2589036085123
-   u1, u2 = [], []
-   x1, x2 = [], []
-   for x in x_main:
-      if x <= ksi:
-         u1.append(C1*np.exp(np.sqrt(ksi/(ksi**2 + 2))*x) + C2*np.exp(-np.sqrt(ksi/(ksi**2 + 2))*x) + 1/ksi)
-         x1.append(x)
-      elif x >= ksi:
-         u2.append(C3*np.exp(x) + C4*np.exp(-x) + np.sin(np.pi*ksi)/0.09)
-         x2.append(x)
-   plt.plot(x1, u1,'o-', linewidth = 2.0, label='траектория до точки разрыва')
-   plt.plot(x2, u2,'o-', linewidth = 2.0, label='траетория после точки разрыва')
-   plt.legend()
-   plt.xlabel('x')
-   plt.ylabel('u(x)')
-   plt.grid()
-   plt.savefig('График истинной траектории.png')
-   plt.show()
-   return u1 + u2
-
-#! Коэффициенты тестовой задачи
-def test_coef(x_main, x_aux, n, a, d, fi):
-   for i in range(n):
-      if (x_main[i + 1] <= 0.3):
-         a[i] = 2.09
-      elif (x_main[i] >= 0.3):
-         a[i] = 0.09
-      elif x_main[i] < 0.3 < x_main[i + 1]:
-         a[i] = 1/(1/(2*k1(0.3)) + 1/(2*k2(0.3)))
-
-   for i in range(n - 1):
-      if (x_aux[i + 1] <= 0.3):
-         d[i] = 0.3
-      elif (x_aux[i] >= 0.3):
-         d[i] = 0.09
-      elif x_aux[i] < 0.3 < x_aux[i + 1]:
-         d[i] = (q1(0.3) + q2(0.3))/2
-   
-   for i in range(n - 1):
-      if (x_aux[i + 1] <= 0.3):
-         fi[i] = 1
-      elif (x_aux[i] >= 0.3):
-         fi[i] = np.sin(0.3*np.pi)
-      elif x_aux[i] < 0.3 < x_aux[i + 1]:
-         fi[i] = (f1(0.3) + f2(0.3))/2
-   return a, d, fi
-
-#! Тестовая задача
-def test_task(n, x_main, x_aux):
-   a = np.empty(n)
-   d = np.empty(n - 1)
-   fi = np.empty(n - 1)
-   a, d ,fi = test_coef(x_main, x_aux, n, a, d, fi)
-   A, B, C = coef_matrix(n, a, d, fi)
-   v = list(progonka(n, A, B, C, a, d ,fi))
-   return v
-
-#TODO------------------------------------ОСНОВНАЯ ЗАДАЧА------------------------------------
-#! Коэффициенты основной задачи
-def main_coef(x_main, x_aux, n, a, d, fi):
-   for i in range(n):
-      if (x_main[i + 1] <= 0.3):
-         a[i] = k1(x_aux[i])
-      elif (x_main[i] >= 0.3):
-         a[i] = k2(x_aux[i])
-      elif x_main[i] < 0.3 < x_main[i + 1]:
-         a[i] = 1/(n*(((0.3 - x_main[i + 1])/(k1((x_main[i] + 0.3)/2))) + ((x_main[i + 1] - 0.3)/(k2((0.3 + x_main[i + 1])/2)))))
-
-   for i in range(n - 1):
-      if (x_aux[i + 1] <= 0.3):
-         d[i] = q1(x_main[i + 1])
-      elif (x_aux[i] >= 0.3):
-         d[i] = q2(x_main[i + 1])
-      elif x_aux[i] < 0.3 < x_aux[i + 1]:
-         d[i] = n*(q1((x_aux[i] + 0.3)/2)*(0.3 - x_aux[i]) + q2((0.3 + x_aux[i + 1])/2)*(x_aux[i + 1] - 0.3))
-   
-   for i in range(n - 1):
-      if (x_aux[i + 1] <= 0.3):
-         fi[i] = f1(x_main[i + 1])
-      elif (x_aux[i] >= 0.3):
-         fi[i] = f2(x_main[i + 1])
-      elif x_aux[i] < 0.3 < x_aux[i + 1]:
-         fi[i] = n*(f1((x_aux[i] + 0.3)/2)*(0.3 - x_aux[i]) + f2((0.3 + x_aux[i + 1])/2)*(x_aux[i + 1] - 0.3))
-   return a, d, fi
-
-#! Основная задача
-def main_task(n, x_main, x_aux):
-   a = np.empty(n)
-   d = np.empty(n - 1)
-   fi = np.empty(n - 1)
-   a, d ,fi = main_coef(x_main, x_aux, n, a, d, fi)
-   A, B, C = coef_matrix(n, a, d, fi)
-   v = list(progonka(n, A, B, C, a, d ,fi))
-   return v
-
-#? Запуск просчетов
-def solution(h, n):
-   #TODO----------ТЕСТОВАЯ ЧАСТЬ----------
-   x_main = [x_start + i*h for i in range(n + 1)]
-   x_aux = [x_start + (i + 0.5)*h for i in range(n)]
-   v1 = test_task(n, x_main, x_aux)
-   u = true_plot(x_main)
-   u[0] = 1
-   u[-1] = 0
-   v1.insert(0, 1)
-   E = list(np.abs(np.array(u) - np.array(v1)))
-
-   for i in range(len(x_main)):
-      x_main[i] = round(x_main[i], 14)
-   Node = [i for i in range(len(x_main))]
-   data_test = {'№ Узла': Node, 'x': x_main, 'v': v1, 'u': u, '|u - v|': E}
-   data = pd.DataFrame(data = data_test)
-   data.to_csv("data_test.csv", index=False)
-
-   max_error_E = 0
-   for i in range(len(v1)):   
-      if E[i] > max_error_E:
-         max_error_E = E[i]
-   
-   max_E = E.index(max_error_E)
-   x1_max = x_main[max_E]
-   
-   #* График разность численнного и аналитического решения
-   plt.plot(x_main, E)
-   plt.xlabel('x')
-   plt.ylabel('|u - v|')
-   plt.legend()
-   plt.grid()
-   plt.savefig('Разность численнного и аналитического решения.png')
-   plt.show()
-   
-   #TODO----------ОСНОВНАЯ ЧАСТЬ----------
-   x_main = [x_start + i*h for i in range(n + 1)]
-   x_aux = [x_start + (i + 0.5)*h for i in range(n)]
-   v1 = main_task(n, x_main, x_aux)
-   n = int(2*n)
-   h = h/2
-   x_main = [x_start + i*h for i in range(n + 2)]
-   x_aux = [x_start + (i + 0.5)*h for i in range(n + 1)]
-   v2 = list(main_task(n, x_main, x_aux))
-   v1.insert(0, 1)
-   v2.insert(0, 1)
-   v2 = v2[::2]
-   e = np.abs(np.array(v1) - np.array(v2))
-   
-   for i in range(len(x_main)):
-      x_main[i] = round(x_main[i], 14)
-   Node = [i for i in range(len(x_main))][::2]
-   data_test = {'№ Узла': Node, 'x': x_main[::2], 'v1': v1, 'v2': v2, '|v2 - v1|': e}
-   data = pd.DataFrame(data = data_test)
-   data.to_csv("data_main.csv", index=False)
-
-   e = list(e)
-   max_error_e = 0
-   for i in range(len(v1)):
-      if e[i] > max_error_e:
-         max_error_e = e[i]
-   
-   max_e = e.index(max_error_e)
-   x2_max = x_main[max_e]
-   
-   #* График численнного и афро-численного решения
-   plt.plot(x_main[::2], v1)
-   plt.plot(x_main[::2], v2, 'x-')
-   plt.xlabel('x')
-   plt.ylabel('v1(x)   v2(x)')
-   plt.legend()
-   plt.grid()
-   plt.savefig('Численный траетории.png')
-   plt.show()
-
-   #* График разности численнного и афро-численного решения
-   plt.plot(x_main[::2], e)
-   plt.xlabel('x')
-   plt.ylabel('|v1 - v2|')
-   plt.legend()
-   plt.grid()
-   plt.savefig('Разность численнного решения и численного с половинным шагом.png')
-   plt.show()
-
-   print("Справка")
-   print('Максимальная погрешность тестовой задачи: ', max_error_E, '  ', 'при x = ', x1_max)
-   print('Максимальная погрешность основной задачи: ', max_error_e, '  ', 'при x = ', x2_max)
 
 
 #* Терминал
-print('_____________________________________________________________________________________________________________________')
+print('________________________________________________________________________________________________')
 print('Команда Эльвина | Лабораторная работа №2 | Решение краевых задач для ОДУ')
-print('_____________________________________________________________________________________________________________________')
+print('________________________________________________________________________________________________')
 print('В рамках данной лабораторной работы решается тествая и основная задачи с определенной сеткой.')
 print('Задайте размерность сетки')
 n = int(input('n = '))
-
-#* Результат работы
-x_start = 0
-x_end = 1
-h = 1/n
-kappa = [0, 0]
-my = [1, 0]
-ksi = 0.3
-
-solution(h, n)
+print("Справка")
+test_task(n)
+main_task(n)
+print('Конец работы программы')
+print('________________________________________________________________________________________________')
